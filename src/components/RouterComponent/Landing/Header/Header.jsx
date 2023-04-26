fd383b92ad857b29c705cd04d05fa1c63622efe1
Garalez <vladgalcev03@gmail.com>
2023-04-26 14:34:34 +0300

    wip bugfix navigate

--- src/components/RouterComponent/Landing/Header/Header.jsx
+++ src/components/RouterComponent/Landing/Header/Header.jsx
@@ -4,22 +4,21 @@ import { ReactComponent as LogoSvg } from '../../../../assets/svg/violetLogo.svg
 import useWindowDimensions from '../../../../hooks/screenViewPort';
 import BurgerMenu from './BurgerMenu';
 import Menu from './Menu';
-import { useNavigate } from 'react-router-dom';
 
 export const Header = () => {
   const { width } = useWindowDimensions();
-  const navigate = useNavigate();
 
   return (
     <header>
       <div className={style.header}>
         <div className={style.headerWrapper}>
-          <div
-            onClick={() => navigate('/application')}
+          <a
+            href='/application'
+            aria-label='Логотип и переход к приложению'
             className={style.logoWrapper}
           >
             <LogoSvg className={style.logo} /> 7-Monet
-          </div>
+          </a>
           {width <= 768 ? <BurgerMenu /> : <Menu />}
         </div>
       </div>
