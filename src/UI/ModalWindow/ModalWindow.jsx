63cea453913040e93956cc82b1eda4137f1ce325
Garalez <vladgalcev03@gmail.com>
2023-05-01 20:00:23 +0300

    wip

--- src/UI/ModalWindow/ModalWindow.jsx
+++ src/UI/ModalWindow/ModalWindow.jsx
@@ -9,19 +9,21 @@ export const ModalWindow = ({ content, modalConfirmAction, closeModal }) => {
 
   return (
     <div className={style.modalOverlay}>
-      <div className={style.modalBody}>
-        <div className={style.modalContentWrapper}>
-          <div className={style.modalContent}>{content}</div>
-          <div className={style.modalBtnsGroup}>
-            <button className={style.modalBtn} onClick={handleModalConfirm}>
-              Подтвердить
-            </button>
-            <button
-              className={style.modalBtn}
-              onClick={() => closeModal()}
-            >
-              Отменить
-            </button>
+      <div className={style.modal}>
+        <div className={style.modalBody}>
+          <div className={style.modalContentWrapper}>
+            <div className={style.modalContent}>{content}</div>
+            <div className={style.modalBtnsGroup}>
+              <button className={style.modalBtn} onClick={handleModalConfirm}>
+                Подтвердить
+              </button>
+              <button
+                className={style.modalBtn}
+                onClick={() => closeModal()}
+              >
+                Отменить
+              </button>
+            </div>
           </div>
         </div>
       </div>
